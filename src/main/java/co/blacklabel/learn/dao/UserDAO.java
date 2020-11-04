6603748288e0498466b90133253a1cd0de906375
Kevin Toro <kevinandrestg7@gmail.com>
2020-11-04 17:47:17 -0500

    create Java EE project, add MVC model, create UserDAO, create User Model

---
+++ src/main/java/co/blacklabel/learn/dao/UserDAO.java
@@ -0,0 +1,167 @@
+package co.blacklabel.learn.dao;
+
+import co.blacklabel.learn.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class for control the User data
+ */
+public class UserDAO {
+  private String jdbcURL = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+  private String dbUser = "root";
+  private String dbPassword = "KAtg1090528922";
+
+  private static final String INSERT_USERS_SQL = "INSERT INTO users(user_name, user_email, user_country)"
+          +"VALUES (?,?,?);";
+  private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+  private static final String SELECT_ALL_USERS = "select * from users";
+  private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+  private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+
+  /**
+   * Make the Connection to the database using the url, user, password given in config values
+   * @return Connection if that's return ok, else null
+   */
+  private Connection getConnection(){
+    Connection connection = null;
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+    } catch (ClassNotFoundException | SQLException exception){
+      System.out.println(exception);
+    }
+    return connection;
+  }
+
+  /**
+   * get the JDBC URL
+   * @return jdbcURL String data
+   */
+  public String getJdbcURL() {
+    return jdbcURL;
+  }
+
+  /**
+   * replace the jdbc URL
+   * @param jdbcURL new url for connection
+   */
+  public void setJdbcURL(String jdbcURL) {
+    this.jdbcURL = jdbcURL;
+  }
+
+  /**
+   * get the saved username from database
+   * @return dbUser String with the database username
+   */
+  public String getDbUser() {
+    return dbUser;
+  }
+
+  /**
+   * updates the username for the database connection
+   * @param dbUser String with the new username
+   */
+  public void setDbUser(String dbUser) {
+    this.dbUser = dbUser;
+  }
+
+  /**
+   * get the database config password
+   * @return dbPassword, string with the password connection
+   */
+  public String getDbPassword() {
+    return dbPassword;
+  }
+
+  /**
+   * set the password connection
+   * @param dbPassword String new password for database
+   */
+  public void setDbPassword(String dbPassword) {
+    this.dbPassword = dbPassword;
+  }
+
+  /**
+   * searches within the database for the user with the identifier given by parameters
+   * @param userId User id that is gonna be search
+   * @return User if exist, else null
+   */
+  public User getUserByID(int userId){
+    User user = null;
+    try{
+      PreparedStatement ps = getConnection().prepareStatement(SELECT_USER_BY_ID);
+      ps.setInt(1, userId);
+      ResultSet rs = ps.executeQuery();
+      while (rs.next()){
+        user = new User(
+                rs.getInt("user_id"),
+                rs.getString("user_name"),
+                rs.getString("user_email"),
+                rs.getString("user_country")
+        );
+      }
+    } catch (SQLException e){
+      System.out.println(e);
+    }
+    return user;
+  }
+
+  /**
+   * executes MySQL query to get all in table users
+   * @return users a list with all the users
+   */
+  public List<User> selectAllUsers(){
+    List<User> users = new ArrayList<>();
+    try {
+      Connection connection = getConnection();
+      PreparedStatement ps = connection.prepareStatement(SELECT_ALL_USERS);
+      ResultSet rs = ps.executeQuery();
+
+      while (rs.next()){
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String email = rs.getString("email");
+        String country = rs.getString("country");
+        users.add(new User(id, name, email, country));
+      }
+    } catch (SQLException e){
+      System.out.println(e);
+    }
+    return users;
+  }
+
+  /**
+   * Delete from the database the user with the ID given
+   * @param id int with the id user
+   * @return true if success, else, false
+   * @throws SQLException if SQL error
+   */
+  public boolean deleteUser(int id) throws SQLException {
+    boolean rowDeleted;
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);
+    statement.setInt(1, id);
+    rowDeleted = statement.executeUpdate() > 0;
+    return rowDeleted;
+  }
+
+  /**
+   * Updates the user data
+   * @param user User whose data will be updated
+   * @return true if success, else false
+   * @throws SQLException if SQL error
+   */
+  public boolean updateUser(User user) throws SQLException {
+    boolean rowUpdated;
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);
+    statement.setString(1, user.getUser_name());
+    statement.setString(2, user.getUser_email());
+    statement.setString(3, user.getUser_country());
+    statement.setInt(4, user.getUser_id());
+    return statement.executeUpdate() > 0;
+  }
+}
---
+++ src/main/java/co/blacklabel/learn/model/User.java
@@ -0,0 +1,60 @@
+package co.blacklabel.learn.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+  private int user_id;
+  private String user_name;
+  private String user_email;
+  private String user_country;
+
+  public User(){
+    super();
+  }
+  public User(String user_name, String user_email, String user_country){
+    super();
+    this.user_name = user_name;
+    this.user_email = user_email;
+    this.user_country = user_country;
+  }
+
+  public User(int user_id, String user_name, String user_email, String user_country) {
+    super();
+    this.user_id = user_id;
+    this.user_name = user_name;
+    this.user_email = user_email;
+    this.user_country = user_country;
+  }
+
+  public int getUser_id() {
+    return user_id;
+  }
+
+  public void setUser_id(int user_id) {
+    this.user_id = user_id;
+  }
+
+  public String getUser_name() {
+    return user_name;
+  }
+
+  public void setUser_name(String user_name) {
+    this.user_name = user_name;
+  }
+
+  public String getUser_email() {
+    return user_email;
+  }
+
+  public void setUser_email(String user_email) {
+    this.user_email = user_email;
+  }
+
+  public String getUser_country() {
+    return user_country;
+  }
+
+  public void setUser_country(String user_country) {
+    this.user_country = user_country;
+  }
+}
